80339b3031c4c7bb878ae92dd6d9181dd68db891
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2017-04-24 01:38:32 +0300

    Various changes
    - Update eslint config.
    - Upgrade scripts to build and watch scss files.
    - Update react-ga.
    - Use SCSS.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './views/app';
 
-// Base styles.
-import './views/styles/index.css';
-
-// Main App styles.
-import './views/styles/app/App.css';
-
-// Components styles.
-import './views/styles/components/KeyCode.css';
+// Styles.
+import './views/styles/style.css';
 
 // TODO: JS Meta Keys (CMD of MacOS).
 ReactDOM.render(
